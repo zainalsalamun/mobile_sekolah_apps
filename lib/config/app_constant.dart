aee88cac2e95754c48db7ba31642200cf81cc1e3
zainalsalamun <zainalrtf@gmail.com>
2025-11-25 15:55:28 +0700

    add : app constant

--- lib/config/app_constant.dart
+++ lib/config/app_constant.dart
@@ -1,12 +1,1 @@
-abstract class AppRoutes {
-  static const splash = '/';
-  static const login = '/login';
 
-  static const dashboardSiswa = '/dashboard-siswa';
-  static const dashboardGuru = '/dashboard-guru';
-
-  static const absensi = '/absensi';
-  static const nilai = '/nilai';
-  static const jadwal = '/jadwal';
-  static const pengumuman = '/pengumuman';
-}
