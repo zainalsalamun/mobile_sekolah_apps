52b94250ca037450dbfe166f498ee6dd411ba379
zainalsalamun <zainalrtf@gmail.com>
2025-11-27 12:28:55 +0700

    Update :  dashboard guru view

--- lib/modules/dashboard_guru/views/dashboard_guru_view.dart
+++ lib/modules/dashboard_guru/views/dashboard_guru_view.dart
@@ -1,158 +1,293 @@
 import 'package:flutter/material.dart';
-import '../../../core/widgets/app_card.dart';
-import '../../../core/widgets/section_header.dart';
+import 'package:get/get.dart';
+import 'package:mobile_sekolah_apps/modules/dashboard_guru/controller/dashboard_guru_controller.dart';
 import '../../../config/app_colors.dart';
+import '../../../core/widgets/app_card.dart';
 
-class DashboardGuruView extends StatelessWidget {
+class DashboardGuruView extends GetView<DashboardGuruController> {
   const DashboardGuruView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Dashboard Guru")),
-
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            /// GREETING
-            Text(
-              "Selamat pagi, Pak Andi 👋",
-              style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-            ),
-
-            const SizedBox(height: 6),
-            const Text(
-              "Guru Matematika",
-              style: TextStyle(color: AppColors.textMedium),
-            ),
-
-            const SizedBox(height: 20),
-
-            /// JADWAL HARI INI
-            const SectionHeader(title: "Jadwal Hari Ini"),
-            const SizedBox(height: 12),
-
-            AppCard(
-              onTap: () {},
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text(
-                    "07:00 - 08:30",
-                    style: TextStyle(color: AppColors.textMedium),
+      backgroundColor: AppColors.background,
+      body: Obx(() {
+        return SingleChildScrollView(
+          child: Column(
+            children: [
+              //header
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.fromLTRB(20, 60, 20, 30),
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Color(0xFF4A80F0), Color(0xFF6BC7FF)],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
                   ),
-                  SizedBox(height: 4),
-                  Text(
-                    "10 IPA 1",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-                  ),
-                  Text(
-                    "Matematika",
-                    style: TextStyle(color: AppColors.textMedium),
-                  ),
-                ],
-              ),
-            ),
-
-            const SizedBox(height: 12),
-
-            AppCard(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text(
-                    "08:30 - 10:00",
-                    style: TextStyle(color: AppColors.textMedium),
-                  ),
-                  SizedBox(height: 4),
-                  Text(
-                    "10 IPA 2",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-                  ),
-                  Text(
-                    "Matematika",
-                    style: TextStyle(color: AppColors.textMedium),
-                  ),
-                ],
-              ),
-            ),
-
-            const SizedBox(height: 24),
-
-            /// LIST KELAS YANG DIAMPU
-            const SectionHeader(title: "Kelas yang Diampu"),
-            const SizedBox(height: 12),
-
-            Row(
-              children: [
-                Expanded(
-                  child: AppCard(
-                    onTap: () {},
-                    child: const Center(
-                      child: Text("10 IPA 1", style: TextStyle(fontSize: 16)),
-                    ),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(28),
+                    bottomRight: Radius.circular(28),
                   ),
                 ),
-                const SizedBox(width: 12),
-                Expanded(
-                  child: AppCard(
-                    onTap: () {},
-                    child: const Center(
-                      child: Text("10 IPA 2", style: TextStyle(fontSize: 16)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    // Profile
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 32,
+                          backgroundColor: Colors.white.withOpacity(0.2),
+                          child: const Icon(
+                            Icons.person,
+                            size: 40,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(width: 16),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              controller.nama.value,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 22,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                            Text(
+                              controller.mapel.value,
+                              style: TextStyle(
+                                color: Colors.white.withOpacity(0.8),
+                                fontSize: 14,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
+
+                    // Notification
+                    Stack(
+                      children: [
+                        const Icon(
+                          Icons.notifications_none_rounded,
+                          color: Colors.white,
+                          size: 28,
+                        ),
+                        Positioned(
+                          right: 0,
+                          top: 0,
+                          child: Container(
+                            width: 10,
+                            height: 10,
+                            decoration: const BoxDecoration(
+                              color: Colors.red,
+                              shape: BoxShape.circle,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 22),
+
+              //kelas absensi
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: AppCard(
+                  padding: const EdgeInsets.all(18),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Kelas Perlu Absensi",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      const SizedBox(height: 12),
+                      Text(
+                        "${controller.kelasUntukAbsensi['mapel']} - ${controller.kelasUntukAbsensi['kelas']}",
+                        style: const TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        controller.kelasUntukAbsensi['jam'] ?? "",
+                        style: const TextStyle(
+                          color: AppColors.textMedium,
+                          fontSize: 14,
+                        ),
+                      ),
+                      const SizedBox(height: 14),
+
+                      ElevatedButton(
+                        onPressed: () => Get.toNamed("/absensi"),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: AppColors.primary,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 16,
+                            vertical: 12,
+                          ),
+                        ),
+                        child: const Text(
+                          "Input Absensi",
+                          style: TextStyle(fontSize: 15),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
-
-            const SizedBox(height: 24),
-
-            /// SHORTCUT ACTION
-            const SectionHeader(title: "Aksi Cepat"),
-            const SizedBox(height: 12),
-
-            AppCard(
-              onTap: () {},
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Text(
-                    "Ambil Absensi",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-                  ),
-                  Icon(
-                    Icons.arrow_forward_ios,
-                    size: 18,
-                    color: AppColors.textMedium,
-                  ),
-                ],
               ),
-            ),
 
-            const SizedBox(height: 12),
+              const SizedBox(height: 26),
 
-            AppCard(
-              onTap: () {},
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Text(
-                    "Input Nilai",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-                  ),
-                  Icon(
-                    Icons.arrow_forward_ios,
-                    size: 18,
-                    color: AppColors.textMedium,
-                  ),
-                ],
+              //jadwal
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Jadwal Mengajar Hari Ini",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 14),
+
+                    ...controller.jadwalHariIni.map((item) {
+                      return AppCard(
+                        margin: const EdgeInsets.only(bottom: 12),
+                        padding: const EdgeInsets.all(16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  item["icon"] ?? "📘",
+                                  style: const TextStyle(fontSize: 22),
+                                ),
+                                const SizedBox(width: 12),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      item["mapel"] ?? "",
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w700,
+                                      ),
+                                    ),
+                                    Text(
+                                      item["kelas"] ?? "",
+                                      style: const TextStyle(
+                                        color: AppColors.textMedium,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Text(
+                              item["jam"] ?? "",
+                              style: const TextStyle(
+                                color: AppColors.textMedium,
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    }),
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
-      ),
+
+              const SizedBox(height: 26),
+
+              //Pengumuman
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Pengumuman Terbaru",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+
+                    ...controller.pengumuman.map((item) {
+                      return AppCard(
+                        margin: const EdgeInsets.only(bottom: 12),
+                        padding: const EdgeInsets.all(18),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                color: AppColors.primary.withOpacity(0.15),
+                                shape: BoxShape.circle,
+                              ),
+                              child: const Icon(
+                                Icons.campaign_rounded,
+                                size: 22,
+                                color: AppColors.primary,
+                              ),
+                            ),
+                            const SizedBox(width: 14),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    item["judul"] ?? "",
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 6),
+                                  Text(
+                                    item["tanggal"] ?? "",
+                                    style: const TextStyle(
+                                      fontSize: 13,
+                                      color: AppColors.textMedium,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    }),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 40),
+            ],
+          ),
+        );
+      }),
     );
   }
 }
