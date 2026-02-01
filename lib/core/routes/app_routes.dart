d8b264a6a44da91d94dd1607a037d27c4036e75f
zainalsalamun <zainalrtf@gmail.com>
2026-02-01 21:22:08 +0700

    update : routes

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -6,9 +6,12 @@ abstract class AppRoutes {
   static const dashboardGuru = '/dashboard-guru';
 
   static const absensi = '/absensi';
+  static const absensiSiswa = '/absensi-siswa';
   static const nilai = '/nilai';
   static const detailNilai = '/detail-nilai';
   static const jadwal = '/jadwal';
   static const pengumuman = '/pengumuman';
   static const pengumumanDetail = '/pengumuman-detail';
+  static const setting = '/setting';
+  static const profile = '/profile';
 }
