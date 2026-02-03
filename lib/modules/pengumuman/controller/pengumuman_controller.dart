bc38d0513e3b2f50564f3fff9002ecc7fee15182
zainalsalamun <zainalrtf@gmail.com>
2026-02-03 09:49:07 +0700

    update : function loadPengumumanList

--- lib/modules/pengumuman/controller/pengumuman_controller.dart
+++ lib/modules/pengumuman/controller/pengumuman_controller.dart
@@ -1,19 +1,29 @@
+import 'dart:convert';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 class PengumumanController extends GetxController {
-  var pengumumanList =
-      [
-        {
-          "judul": "Ulangan Umum Dimulai",
-          "tanggal": "12 Nov 2025",
-          "isi":
-              "Ulangan umum semester ganjil akan dimulai pada Senin. Harap dipersiapkan.",
-        },
-        {
-          "judul": "Libur Akhir Semester",
-          "tanggal": "05 Des 2025",
-          "isi":
-              "Libur semester dimulai tanggal 20. Aktivitas sekolah kembali normal pada 5 Januari.",
-        },
-      ].obs;
+  var pengumumanList = <Map<String, dynamic>>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    loadPengumumanList();
+  }
+
+  void loadPengumumanList() async {
+    try {
+      final String response = await rootBundle.loadString(
+        'assets/data/pengumuman.json',
+      );
+      final List<dynamic> data = jsonDecode(response);
+
+      // Sort by id descending to show newest first
+      data.sort((a, b) => (b['id'] as int).compareTo(a['id'] as int));
+
+      pengumumanList.value = data.cast<Map<String, dynamic>>();
+    } catch (e) {
+      print("Error loading pengumuman: $e");
+    }
+  }
 }
