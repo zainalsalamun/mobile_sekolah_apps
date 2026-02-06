2af15bbc5e722362ecb561b1c6f861cb98a0d53d
zainalsalamun <zainalrtf@gmail.com>
2026-02-06 11:26:10 +0700

    feat: implement interactive calendar for date-based schedule viewing in Jadwal module

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:mobile_sekolah_apps/config/app_theme.dart';
 import 'package:mobile_sekolah_apps/core/bindings/initial_binding.dart';
@@ -7,6 +8,7 @@ import 'package:mobile_sekolah_apps/core/routes/app_routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('id_ID', null);
 
   runApp(
     GetMaterialApp(
