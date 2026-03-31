3f2b65f6041e0effb6aed9c7cb7c1dd2ff706d30
zainalsalamun <zainalrtf@gmail.com>
2026-03-31 11:06:21 +0700

    feat: Refactor NilaiController to manage full and filtered mapel lists with semester selection

--- lib/modules/nilai/controller/nilai_controller.dart
+++ lib/modules/nilai/controller/nilai_controller.dart
@@ -3,7 +3,16 @@ import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 class NilaiController extends GetxController {
-  var mapelList = <Map<String, dynamic>>[].obs;
+  var fullMapelList = <Map<String, dynamic>>[].obs;
+  var filteredMapelList = <Map<String, dynamic>>[].obs;
+
+  final List<String> semesterList = [
+    "Semua Semester",
+    "Semester 1",
+    "Semester 2",
+  ];
+
+  RxString selectedSemester = "Semua Semester".obs;
 
   @override
   void onInit() {
@@ -17,14 +26,33 @@ class NilaiController extends GetxController {
         'assets/data/nilai_siswa.json',
       );
       final List<dynamic> data = jsonDecode(response);
-      mapelList.value = data.cast<Map<String, dynamic>>();
+      fullMapelList.value = data.cast<Map<String, dynamic>>();
+      filterNilai();
     } catch (e) {
       print("Error loading nilai: $e");
     }
   }
 
+  void changeSemester(String semester) {
+    selectedSemester.value = semester;
+    filterNilai();
+  }
+
+  void filterNilai() {
+    if (selectedSemester.value == "Semua Semester") {
+      filteredMapelList.value = List.from(fullMapelList);
+      return;
+    }
+
+    int semesterNumber = int.parse(selectedSemester.value.split(" ").last);
+    filteredMapelList.value =
+        fullMapelList.where((item) {
+          return item["semester"] == semesterNumber;
+        }).toList();
+  }
+
   Map<String, dynamic> getDetailNilai(String mapelName) {
-    return mapelList.firstWhere(
+    return filteredMapelList.firstWhere(
       (element) => element['nama'] == mapelName,
       orElse: () => {},
     );
