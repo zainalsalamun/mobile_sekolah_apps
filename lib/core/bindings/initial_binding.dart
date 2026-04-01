5c599b4f6994bee63c385a1d7d00d690f14954ed
zainalsalamun <zainalrtf@gmail.com>
2026-04-01 14:22:16 +0700

    feat: Update InitialBinding to include LoginController as a permanent dependency

--- lib/core/bindings/initial_binding.dart
+++ lib/core/bindings/initial_binding.dart
@@ -1,8 +1,10 @@
-import 'package:get/get_instance/src/bindings_interface.dart';
+import 'package:get/get.dart';
+import 'package:mobile_sekolah_apps/modules/auth/controller/login_controller.dart';
 
 class InitialBinding extends Bindings {
   @override
   void dependencies() {
+    Get.put(LoginController(), permanent: true);
     // Get.put(ApiService());
     // Get.put(AuthenticationService());
   }
