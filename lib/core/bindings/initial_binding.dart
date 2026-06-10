f5588048045dc0a041fbf3004e68855bf79faedc
zainalsalamun <zainalrtf@gmail.com>
2026-06-10 14:21:18 +0700

    feat: add ApiProvider to initial bindings
    This change integrates ApiProvider as a permanent dependency in the initial bindings for streamlined API functionality, replacing commented-out service declarations.

--- lib/core/bindings/initial_binding.dart
+++ lib/core/bindings/initial_binding.dart
@@ -1,11 +1,11 @@
 import 'package:get/get.dart';
 import 'package:mobile_sekolah_apps/modules/auth/controller/login_controller.dart';
+import 'package:mobile_sekolah_apps/data/providers/api_provider.dart';
 
 class InitialBinding extends Bindings {
   @override
   void dependencies() {
+    Get.put(ApiProvider.instance, permanent: true);
     Get.put(LoginController(), permanent: true);
-    // Get.put(ApiService());
-    // Get.put(AuthenticationService());
   }
 }
